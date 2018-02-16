c47cac02f948d1b35c91f4641280920cb6da17d4
Hanan <q.hanan2727@gmail.com>
2018-02-17 00:44:32 +0300

    second testing

---
+++ app/src/main/java/com/example/hanan/eduhr_application/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hanan.eduhr_application
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
