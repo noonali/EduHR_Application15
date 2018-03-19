060a33196cde38ff1717334efd293de491390e05
Hanan <q.hanan2727@gmail.com>
2018-03-19 03:38:31 +0300

    with Data Base connection for new teacher

--- app/src/main/java/com/example/hanan/eduhr_application/HomeActivity.kt
+++ app/src/main/java/com/example/hanan/eduhr_application/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.example.hanan.eduhr_application
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent;
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btnNextActivity.setOnClickListener() {
+            val intent = Intent(this, ChooseUserType::class.java)
+            startActivity(intent)
+
+        }
     }
 }
